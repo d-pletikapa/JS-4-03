3b90a16286476b4cba84c8920f4f84c07c483922
Dennys <keta548@vivaldi.net>
2022-08-30 11:05:04 +0300

    init

---
+++ script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const productName = 'Samsung S20-FE';
+const productCount = 2;
+const productCategory = 'Смартфоны';
+const productPrice = 45000;
+const totallPrice = productPrice * productCount;
+
+console.log('Имя товара: ' + productName);
+console.log('Категория: ' + productCategory);
+console.log('Количество товара: ' + productCount);
+console.log('Итого: ' + totallPrice + ' руб.');
