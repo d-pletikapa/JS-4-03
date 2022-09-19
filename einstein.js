04340234f24ecd5c61cc757a20cc5927fa28efde
keta548 <keta548@vivaldi.net>
2022-09-19 18:28:11 +0300

    updt pratice

--- einstein.js
+++ einstein.js
@@ -1,8 +1,46 @@
-console.log(`Доп. задание`)
+'use strict';
+const highlight = 'padding: 0 20px; color: darkblue'
+
+console.log(`%cДоп. задание`, highlight)
 const myWeight = 91n;
 console.log (`myWeight ${typeof myWeight} = ${myWeight}`)
 const speedLight = BigInt(3e8);
 console.log (`speedLight ${typeof speedLight} = ${speedLight}`)
 const bodyEnergy = BigInt(myWeight * speedLight**2n);
+// const bodyEnergy = 91 * (Math.pow(300000000, 2));
 console.log(`Считаем по формуле: (myWeight * speedLight**2n)`);
-console.log(`Энергия тела равна ${bodyEnergy} джоулей`);
+const total = `Энергия тела равна ${bodyEnergy} джоулей`;
+console.log(total);
+console.log(total.concat(' !!!', ' !@#'));
+console.log('total.length (str length)', total.length);
+console.log(total.toUpperCase());
+console.log(total.toLowerCase());
+console.log('letter index in string total.charAt(0) ', total.charAt(0),);
+console.log('letter index in string total[0] ', total[0],);
+console.log('total.substring(7), total.substring(7, 12)', total.substring(7), total.substring(7, 12));
+console.log('total.slice(-5), total.slice(7, 12)', total.slice(-5), total.slice(7, 12));
+console.log(`total.indexOf('Э')`, total.indexOf('Э'));
+console.log(`total.indexOf('тела')`, total.indexOf('тела'));
+console.log(`total.includes('тела')`, total.includes('тела'));
+console.log(`total.replace('тела')`, total.replace('тела', 'объекта'));
+console.log(`total.split(' ')`, total.split(' '));
+console.log(`(убрать лишние пробелы) total.trim()`, total.trim());
+console.log(`total.padStart()`, total.padStart(48, '#'));
+console.log(`total.padEnd()`, total.padEnd(55, 'Y'));
+
+console.log('Math.floor(5.99): ', Math.floor(5.99));
+console.log('Math.ceil(5.99): ', Math.ceil(5.99));
+console.log('Math.round(5.6): ', Math.round(5.6));
+console.log('Math.round(5.4): ', Math.round(5.4));
+console.log('Math.trunc(5.99): ', Math.trunc(5.99));
+console.log('Math.trunc(5.11): ', Math.trunc(5.11));
+const max = Math.max(14,19,12,44,25);
+const min = Math.min(14,19,12,44,25);
+console.log('Math.max: ', max);
+console.log('Math.min: ', min);
+const rnd = Math.random();
+console.log('Math.random: (0-1) ', rnd);
+const rndR = Math.random() * 10;
+console.log('Math.random: (0-1) ', Math.round(rndR));
+
+console.log ('https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random');
--- script.js
+++ script.js
@@ -1,15 +1,36 @@
 'use strict';
+const rainbow = 'padding: 0 20px; color: darkgrey'
 
 const productName = prompt ('Введите наименование товара', ['Samsung S20-FE']);
 let productCount = prompt ('Введите количество товара', [2]);
+
 console.log ('productCount is number?', typeof productCount);
+console.log (`productCount Number.isNaN(${productCount})`, Number.isNaN(productCount));
+console.log (`productCount Number.isFinite(${productCount})`, Number.isFinite(productCount));
+console.log (`productCount Number.isInteger(${productCount})`, Number.isInteger(productCount));
+
 if (typeof productCount !== Number) {productCount = Number(productCount)}
+
 console.log ('And now productCount is number?', typeof productCount);
+console.log (`productCount Number.isNaN(${productCount})`, Number.isNaN(productCount));
+console.log (`productCount Number.isFinite(${productCount})`, Number.isFinite(productCount));
+console.log (`productCount Number.isInteger(${productCount})`, Number.isInteger(productCount));
+
 const productCategory = prompt ('Введите категорию товара', ['Смартфоны']);
 let productPrice = prompt ('Введите стоимость товара', [45000]);
-console.log ('productPrice is number?', typeof productPrice);
-if (typeof productPrice !== Number) {productPrice = Number(productPrice)}
-console.log ('And now productPrice is number?', typeof productPrice);
+
+console.log ('%cproductPrice is number?', rainbow, typeof productPrice);
+console.log (`%cproductPrice Number.isNaN(${productPrice})`, rainbow, Number.isNaN(productPrice));
+console.log (`%cproductPrice Number.isFinite(${productPrice})`, rainbow, Number.isFinite(productPrice));
+console.log (`%cproductPrice Number.isInteger(${productPrice})`, rainbow, Number.isInteger(productPrice));
+
+if (typeof productPrice !== Number) {productPrice = +productPrice}
+
+console.log ('%cAnd now productPrice is number?', rainbow, typeof productPrice);
+console.log (`%cproductPrice Number.isNaN(${productPrice})`, rainbow, Number.isNaN(productPrice));
+console.log (`%cproductPrice Number.isFinite(${productPrice})`, rainbow, Number.isFinite(productPrice));
+console.log (`%cproductPrice Number.isInteger(${productPrice})`, rainbow, Number.isInteger(productPrice));
+
 const totallPrice = productPrice * productCount;
 
 
